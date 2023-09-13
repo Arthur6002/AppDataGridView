36c78f33a03cbd72b777610a19f74ae82504994b
Arthur6002 <arthurcesardefreitas@gmail.com>
2023-09-13 10:48:44 -0300

    Inicio

---
+++ AppDataGrid/AppDataGrid/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppDataGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvAlunos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            dgvAlunos.Rows.Add(txtNome.Text, txtCurso.Text);
+
+            txtNome.Clear();
+            txtCurso.Clear();
+
+            MessageBox.Show("Aluno incluido com sucesso", "Inclusão", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            lblTotal.Text = dgvAlunos.RowCount.ToString();
+        }
+    }
+}
